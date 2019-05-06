ba45dd3fa550141f3e7721313c3d232c370964d7
BizkaiaBasketG2 <46705284+BizkaiaBasketG2@users.noreply.github.com>
2019-05-06 14:45:56 +0200

    version 0.31a
    Control de excepción de persistencia al no encontrar entidades en la
    BBDD objectDB para crear una nueva por defecto.

--- src/DAL/Objectdbdal.java
+++ src/DAL/Objectdbdal.java
@@ -29,26 +29,47 @@ public class Objectdbdal {
 			// Creamos una consulta para obtener el número de usuarios registrados en el sistema.
 			Query numeroUsuariosQuery = em.createQuery("SELECT COUNT(usuario) FROM Usuario usuario");
 			
-			// Ejecutamos la consulta y guardamos su resultado en una variable.
-			long numeroUsuarios = (long) numeroUsuariosQuery.getSingleResult();
-			// Si no existe ningun usuario registrado en el sistema:
-			if (numeroUsuarios == 0) {
-				// Creamos la cuenta por defecto.
-				Usuario admin = new Usuario("admin", "admin");
-				// Ponemos en cola la persistencia del nuevo usuario.
-				em.persist(admin);
-				// Almacenamos en la base de datos los cambios preparados en la transacción. 
-				em.getTransaction().commit();
-			} 
-			// SOLO PARA PRUEBA: Si existe algun usuario:
-			else {
-				// Muestra la cantidad de usuarios registrados en el sistema.
-				System.out.println("Usuarios registrados: " + numeroUsuarios);
+			
+			try {
+				// Ejecutamos la consulta y guardamos su resultado en una variable.
+				long numeroUsuarios = (long) numeroUsuariosQuery.getSingleResult();
+			
+				// Si no existe ningun usuario registrado en el sistema:
+				if (numeroUsuarios == 0) {
+					// Creamos la cuenta por defecto.
+					Usuario admin = new Usuario("admin", "admin");
+					// Ponemos en cola la persistencia del nuevo usuario.
+					em.persist(admin);
+					// Almacenamos en la base de datos los cambios preparados en la transacción. 
+					em.getTransaction().commit();
+				} 
+				// SOLO PARA PRUEBA: Si existe algun usuario:
+				else {
+					// Muestra la cantidad de usuarios registrados en el sistema.
+					System.out.println("Usuarios registrados: " + numeroUsuarios);
 			}
-		// Si surje alguna excepcion durante la conexión con objectDB:
+		
+
+		
+		}
+			// Si surje alguna excepcion durante la conexión con objectDB:
+			catch (PersistenceException e) {
+		
+					// Creamos la cuenta por defecto.
+					Usuario admin = new Usuario("admin", "admin");
+					// Ponemos en cola la persistencia del nuevo usuario.
+					em.persist(admin);
+					// Almacenamos en la base de datos los cambios preparados en la transacción. 
+					em.getTransaction().commit();
+				
+			// Finalmente:
+			}
+			
+		// Si surje alguna excepcion durante la conexión con objectDB:	
 		} catch (PersistenceException e) {
 			// Lanzar la excepción para controlarla en la siguiente capa.
 			throw e;
+			
 		// Finalmente:
 		} finally {
 			// Cerramos el enlace con objectDB.
