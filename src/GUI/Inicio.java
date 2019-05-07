8ef2ff4acc086fdbe27dde25655d2f39706fc3d9
ik_1dw3 <ik_1dw3@218-HZ075770.H015110.NET>
2019-05-07 09:03:38 +0200

    version 0.33a
    Prueba recogida datos por teclado

--- src/GUI/Inicio.java
+++ src/GUI/Inicio.java
@@ -1,14 +1,20 @@
 package GUI;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import javax.persistence.PersistenceException;
 
 import DAL.Mysqldal;
 import DAL.Objectdbdal;
+import DAL.OBJECTDB.Usuario;
 
 public class Inicio {
-
+		
+	private static Usuario login;
+	static String u;
+	static String p;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -26,12 +32,32 @@ public class Inicio {
 		catch (SQLException e) {
 			System.out.println("MySQL Conexión incorrecta.");
 			e.printStackTrace();
+			System.exit(0);
 		}
 		// Si surje algun error durante la conexión con objectDB:
 		catch (PersistenceException e) {
 			System.out.println("objectDB Conexión incorrecta.");
 			e.printStackTrace();
+			System.exit(0);		
+		}
+		
+		finally {
+			
+			while (login == null) {
+			
+				Scanner teclado = new Scanner(System.in);
+				System.out.println("Ingrese nombre de usuario:");
+				u = teclado.nextLine();
+				System.out.println("Ingrese contraseña:");
+				p = teclado.nextLine();
+				teclado.close();
+				
+				login = new Usuario(u, p);
+			
+			// System.out.println(u + " " + p);
+				System.out.println(login);
+				
+			}
 		}
 	}
-
 }
